2e47cc750e451cb32a671c2bff08f4655c3ccc58
starryeye <see2818@naver.com>
2024-06-27 21:36:01 +0900

    [starryeye, 2024.06.27] - project init
    내용
    - project init

--- spring6/hellospring/src/main/java/dev/starryeye/hellospring/PaymentService.java
+++ spring6/hellospring/src/main/java/dev/starryeye/hellospring/PaymentService.java
@@ -1,14 +1,31 @@
 package dev.starryeye.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
 
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+
+        /**
+         * todo
+         *  환율 가져오기
+         *  금액 계산
+         *  유효시간 계산
+         */
+        return Payment.builder()
+                .orderId(orderId)
+                .currency(currency)
+                .foreignCurrencyAmount(foreignCurrencyAmount)
+                .exchangeRate(BigDecimal.ZERO)
+                .convertedAmount(BigDecimal.ZERO)
+                .validUntil(LocalDateTime.now())
+                .build();
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.9));
         System.out.println(payment);
     }
 }
